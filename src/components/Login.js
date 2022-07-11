7c894222031ffe5a44d6bb8b4f9ee790a7a41cc6
Kwuewue <jsepulvedato@gmail.com>
2022-07-11 19:22:09 -0400

    Clase 20 api

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,84 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import useAuth from '../helpers/useAuth.js';
+import config from '../helpers/config.json';
 const Login =() =>{
+    const {setAuth} = useAuth();
+    let navigate = useNavigate();
+    const changeButtonState = (button,enabled)=>{
+        if(enabled){
+            button.disabled=false;
+            button.innerHTML="<i class='fa fa-sign-in'></i> Acceder";
+        } else{
+            button.disabled=true;
+            button.innerHTML="<i class='fa fa-spin fa-spinner'></i> Accediendo...";
+        }
+    };
+    const showMessage=(visible,message)=>{
+        const messageBox = document.querySelector('.alert');
+        const reasonBox = document.querySelector("#reason");
+        if(visible){
+            reasonBox.innerHTML=message;
+            messageBox.classList.remove('d-none');
+        }
+        else{
+            reasonBox.innerHTML="";
+            messageBox.classList.add("d-none");
+        }
+    }
+    const logger =async(event)=>{
+        event.preventDefault();
+        const button=document.querySelector('button');
+        changeButtonState(button,false);
+        var {username, pass}=document.forms[0];
+        const user=username.value;
+        const password=pass.value;
+        if(user.length === 0 || password.length === 0 ){
+            showMessage(true,"Debe completar todos los campos");
+            changeButtonState(button,true);
+            return;  
+        }
+        const requestOptions={
+            method:'POST',
+            headers:{'Content-Type': 'application/json'},
+            body: JSON.stringify({nickname:user,password:password, oepratorId: config.operatorId})
+        };
+        fetch(config.apiURL+"login",requestOptions).then((response)=>{
+            switch(response.status){
+                case 403:
+                    showMessage(true,"Acceso prohibido");
+                    changeButtonState(button,true);
+                    break;
+                case 404:
+                    showMessage(true,"Nombre de usuario o clave incorrectos");
+                    changeButtonState(button,true);
+                    break;
+                    default:
+                        //
+            }
+            return response.json();
+         }).then((result)=>{
+            if(!result.data[0].active){
+                showMessage(true,"El Usuario esta inactivo");
+                changeButtonState(button,true);
+                return;
+            }
+            try{
+                const infoData=result.data[0];
+                const infoUser=JSON.stringify(infoData);
+                showMessage(false,"");
+                changeButtonState(button,true);
+                localStorage.setItem("user",infoUser);
+                const roles=[infoData['level']];
+                setAuth({user,password,roles});
+                navigate('/sales');
+            } catch(error){
+                console.log(error);
+            }
+         }).catch((_error)=>{
+            //
+         })
+    };
     return(
         <div className="hold-transition login-page">
         <div className="login-box">
@@ -9,7 +88,7 @@ const Login =() =>{
         <div className="card">
         <div className="card-body login-card-body">
         <p className="login-box-msg">Sign in to start your session</p>
-        <form action="../../index3.html" method="post">
+        <form onSubmit={logger} action="../../index3.html" method="post">
         <div className="input-group mb-3">
         <input type="email" className="form-control" placeholder="Email"/>
         <div className="input-group-append">
@@ -34,6 +113,10 @@ const Login =() =>{
 
         </div>
         </form>
+        <div className="alert alert-danger d-none" role="alert">
+            <strong>Error!</strong>
+            <p id="reason"></p>
+        </div>
 
         </div>
 
